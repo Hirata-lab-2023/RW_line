3639718c075722398ff9f1a632e550b640a197e9
Hirata lab 2023 <141122931+Hirata-lab-2023@users.noreply.github.com>
2025-01-20 16:04:16 +0900

    Add files via upload

---
+++ phy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir vcf/merge
+vcf-merge vcf/fin_vcf/* > vcf/merge/merged.vcf
+python vcf2phylip.py --input merged.vcf
+python ascbias.py -p merged.min4.phy --outfile merged_out.phy
+cat merged_out.phy | sed -e "s/*/-/g" > merged_out_2.phy
+modeltest-ng -i merged_out_2.phy
+raxml-ng --msa merged_out2.phy --model GTR --all --bs-trees 10000 --threads 16 
