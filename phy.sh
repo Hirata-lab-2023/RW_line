53482633410cf4b97178e7cc90142edc1b663524
Hirata lab 2023 <141122931+Hirata-lab-2023@users.noreply.github.com>
2025-03-13 14:32:26 +0900

    Update phy.sh

--- phy.sh
+++ phy.sh
@@ -5,5 +5,5 @@ python vcf2phylip.py --input merged.vcf
 python ascbias.py -p merged.min4.phy --outfile merged_out.phy
 cat merged_out.phy | sed -e "s/*/-/g" > merged_out_2.phy
 modeltest-ng -i merged_out_2.phy
-#raxml-ng --msa merged_out2.phy --model GTR --all --bs-trees 10000 --threads 16
-raxml-ng --msa merged_out2.phy --model GTR+ASC_LEWIS --all --bs-trees 10000 --threads 25
+#raxml-ng --msa merged_out_2.phy --model GTR --all --bs-trees 10000 --threads 16
+raxml-ng --msa merged_out_2.phy --model GTR+ASC_LEWIS --all --bs-trees 10000 --threads 25
